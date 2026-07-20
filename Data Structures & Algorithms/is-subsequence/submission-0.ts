23e77b3d04597230e9909e8caac4d67464606d8f
Peprah Dickson Daniel <techpepson@gmail.com>
2026-07-20 15:50:43 +0000

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s: string, t: string): boolean {
+        let spointer = 0;
+        let tpointer = 0;
+        let match = "";
+
+        while (spointer < s.length && tpointer < t.length) {
+            if (s[spointer] === t[tpointer]){
+                match += t[tpointer]
+                spointer++
+            }
+            tpointer++
+        }
+
+        if (match === s) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
